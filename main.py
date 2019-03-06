a5845e3c2f36d4e503581cf6cc46cd6e50b3398e
Yan Chen <cheny.gary@gmail.com>
2019-03-06 17:49:29 +0800

    添加request 包头

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ header = {
 		'authority': 'billing.virmach.com',
 		'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
 		'accept-encoding': 'gzip, deflate, br',
-		'accept-language: ': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,en-US;q=0.7,en;q=0.',
+		'accept-language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,en-US;q=0.7,en;q=0.',
 		'cache-control':'max-age=0',
 		'upgrade-insecure-requests': '1',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36',
